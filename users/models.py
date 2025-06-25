553c72758167130bd7b60c819d35c3684663aed3
Andrey Egorov <newart525@yandex.ru>
2025-06-25 20:27:02 +0300

    fixes

--- users/models.py
+++ users/models.py
@@ -6,9 +6,7 @@ from django.db import models
 
 class User(AbstractUser):
     email = models.EmailField(unique=True, verbose_name="E-mail")
-    username = models.CharField(
-        max_length=50, unique=True, verbose_name="Имя пользователя"
-    )
+
     first_name = models.CharField(max_length=50, verbose_name="Имя")
     last_name = models.CharField(max_length=50, verbose_name="Фамилия")
     phone_number = models.CharField(max_length=50, verbose_name="Номер телефона")
@@ -18,7 +16,7 @@ class User(AbstractUser):
     )
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["username"]
+    EXCLUDE_FIELDS = ['username']
 
     def __str__(self):
         return f"{self.username} - {self.email}"
