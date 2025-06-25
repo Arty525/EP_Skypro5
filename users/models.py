0704e3d99bc4a43aa46e1d3b0f0721675bb3449d
Andrey Egorov <newart525@yandex.ru>
2025-06-25 20:42:38 +0300

    fixes

--- users/models.py
+++ users/models.py
@@ -1,12 +1,39 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
 
+class CustomUserManager(BaseUserManager):
+    #Для устранения ошибки при создании пользователя без username
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError('The Email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
 
 class User(AbstractUser):
+    username = None
     email = models.EmailField(unique=True, verbose_name="E-mail")
-
     first_name = models.CharField(max_length=50, verbose_name="Имя")
     last_name = models.CharField(max_length=50, verbose_name="Фамилия")
     phone_number = models.CharField(max_length=50, verbose_name="Номер телефона")
@@ -16,7 +43,9 @@ class User(AbstractUser):
     )
 
     USERNAME_FIELD = "email"
-    EXCLUDE_FIELDS = ['username']
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
 
     def __str__(self):
         return f"{self.username} - {self.email}"
