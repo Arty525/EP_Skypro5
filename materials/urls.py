cb5cdf5ef1adac1ab781d554f122e3413b08c856
Andrey Egorov <89473462+Arty525@users.noreply.github.com>
2025-07-10 20:39:04 +0300

    Revert "Feature/homework 32 1"

--- materials/urls.py
+++ materials/urls.py
@@ -6,7 +6,6 @@ from .views import (
     LessonUpdateAPIView,
     LessonRetrieveAPIView,
     LessonDestroyAPIView,
-    CourseSubscribeAPIView,
 )
 
 from rest_framework.routers import DefaultRouter
@@ -26,10 +25,5 @@ urlpatterns = [
     ),
     path(
         "lessons/<int:pk>/update/", LessonUpdateAPIView.as_view(), name="lesson_update"
-    ),
-    path(
-        "courses/<int:pk>/subscribe/",
-        CourseSubscribeAPIView.as_view(),
-        name="course_subscribe",
     ),
 ] + router.urls
